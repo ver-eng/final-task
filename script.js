e811e16d90704d0c01e295c4cd2ec1fe220668b9
ver-eng <v.patsinashvili@gmail.com>
2023-11-05 21:35:14 +0400

    first

---
+++ script.js
@@ -0,0 +1,79 @@
+// "use strict";
+
+// const burgerBtn = document.querySelector(".burger-menu");
+// const planetsList = document.querySelector(".burger-menu-list");
+// const mainDetails = document.querySelector(".main-stuff");
+// console.log(burgerBtn);
+
+// burgerBtn.addEventListener("click", () => {
+//   planetsList.classList.add("active");
+// });
+
+// /*const firstButton = document.querySelector(".btn-one");
+// const secondButton = document.querySelector(".btn-two");
+// const thirddButton = document.querySelector(".btn-three");
+
+// const firstPhoto = document.querySelector(".img-first");
+// const secondPhoto = document.querySelector(".img-second");
+// const thirdPhoto = document.querySelector(".img-third");
+
+// const firstParag = document.querySelector(".planet-description-one");
+// const secondParag = document.querySelector(".planet-description-two");
+// const thirdParag = document.querySelector(".planet-description-three");
+
+// secondButton.addEventListener("click", () => {
+//   secondPhoto.classList.add("active");
+//   secondParag.classList.add("active");
+
+//   // firstPhoto.classList.remove("active");
+//   // firstPhoto.classList.remove("active");
+//   firstPhoto.classList.add("notactive");
+
+//   // // thirdPhoto.classList.add("notactive");
+
+//   // firstParag.classList.remove("active");
+//   firstParag.classList.add("notactive");
+
+//   // thirdParag.classList.add("notactive");
+
+//   if (secondPhoto.classList.contains("active")) {
+//     secondButton.style.background = "#419EBB";
+//   }
+// });
+
+// firstButton.addEventListener("click", () => {
+//   firstPhoto.classList.add("active");
+//   firstParag.classList.add("active");
+
+//   // firstPhoto.classList.remove("active");
+//   // secondPhoto.classList.remove("active");
+//   // secondPhoto.classList.add("notactive");
+
+//   // // thirdPhoto.classList.add("notactive");
+
+//   // firstParag.classList.remove("active");
+//   // firstParag.classList.add("notactive");
+
+//   // thirdParag.classList.add("notactive");
+
+//   if (firstPhoto.classList.contains("active")) {
+//     firstButton.style.background = "#419EBB";
+//   }
+// });
+
+// // firstButton.addEventListener("click", () => {
+// //   firstPhoto.classList.add("active");
+// //   firstParag.classList.add("active");
+
+// //   secondPhoto.classList.remove("active");
+// //   // thirdPhoto.classList.add("notactive");
+
+// //   secondParag.classList.remove("active");
+// //   // thirdParag.classList.add("notactive");
+
+// //   if (firstPhoto.classList.contains("active")) {
+// //     firstButton.style.background = "#419EBB";
+// //     // secondButton.style.background = "transparent";
+// //     // thirdButton.style.background = "transparent";
+// //   }
+// // });*/
